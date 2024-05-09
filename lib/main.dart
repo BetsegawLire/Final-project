d2c99a529b3b239ecf962f5a4752ae0b3a957c1c
DutcheAT <suraatte@gmail.com>
2024-05-09 19:46:05 +0300

    Machinery detail screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:amrpfe/screens/home.dart';
 import 'package:amrpfe/screens/sign_up.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: SignUpScreen(),
+    home: MainScreen(),
+    //home: SignUpScreen(),
     debugShowCheckedModeBanner: false,
   ));
   //Main page
